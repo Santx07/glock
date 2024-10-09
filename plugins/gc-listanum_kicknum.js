59bd62e20e6f7d297fdd9aae2c8080834ae4b764
Santx07 <fabersantiagoyt84@gmail.com>
2024-10-09 14:30:32 -0500

    KillzN

--- plugins/gc-listanum_kicknum.js
+++ plugins/gc-listanum_kicknum.js
@@ -1,5 +1,5 @@
-/*              Codigo Creado Por Bruno Sobrino
-      (https://github.com/BrunoSobrino/TheMystic-Bot-MD)
+/*              Codigo Creado Por KillzN
+      (https://github.com/Santx07/KillzN)
 */
 
 
