bc6256f641114501bac742247f182899ef84a571
KillzN <scorpionsheep203@gmail.com>
2024-10-14 20:14:36 -0300

    KILL - BOT

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ import fs from 'fs';
 import moment from 'moment-timezone';
 global.botnumber = ""
 global.confirmCode = ""
-global.authFile = `KillzNSession`;
+global.authFile = `𝐊𝐈𝐋𝐋𝐙𝐍Session`;
 // Cambiar a true si el Bot responde a sus comandos con otros comandos.
 // Cambiar a false para usar el Bot desde el mismo numero del Bot.
 // Error de m.isBaileys marcado como false fix temporal
